fb8e745318409174ad202cc77f611af5bc7d2398
Sameer Humane <sameergta143@gmail.com>
2026-08-03 11:05:44 +0530

    Update 01_Create_Tables.sql

--- SQL/01_Create_Tables.sql
+++ SQL/01_Create_Tables.sql
@@ -1,1 +1,63 @@
+CREATE TABLE customers (
+    customer_id VARCHAR(50) PRIMARY KEY,
+    customer_unique_id VARCHAR(50),
+    customer_zip_code_prefix INT,
+    customer_city VARCHAR(100),
+    customer_state CHAR(2)
+);
 
+CREATE TABLE orders (
+    order_id VARCHAR(50) PRIMARY KEY,
+    customer_id VARCHAR(50),
+    order_status VARCHAR(30),
+    order_purchase_timestamp DATETIME,
+    order_approved_at DATETIME,
+    order_delivered_carrier_date VARCHAR(50),
+    order_delivered_customer_date VARCHAR(50),
+    order_estimated_delivery_date VARCHAR(50)
+);
+
+CREATE TABLE order_items (
+    order_id VARCHAR(50),
+    order_item_id INT,
+    product_id VARCHAR(50),
+    seller_id VARCHAR(50),
+    shipping_limit_date DATETIME,
+    price DECIMAL(10,2),
+    freight_value DECIMAL(10,2)
+);
+
+CREATE TABLE order_payments (
+    order_id VARCHAR(50),
+    payment_sequential INT,
+    payment_type VARCHAR(30),
+    payment_installments INT,
+    payment_value DECIMAL(10,2)
+);
+
+CREATE TABLE products (
+    product_id VARCHAR(50) PRIMARY KEY,
+    product_category_name VARCHAR(100),
+    product_name_length VARCHAR(50),
+    product_description_length VARCHAR(50),
+    product_photos_qty VARCHAR(50),
+    product_weight_g VARCHAR(50),
+    product_length_cm VARCHAR(50),
+    product_height_cm VARCHAR(50),
+    product_width_cm VARCHAR(50)
+);
+
+CREATE TABLE sellers (
+    seller_id VARCHAR(50) PRIMARY KEY,
+    seller_zip_code_prefix INT,
+    seller_city VARCHAR(100),
+    seller_state CHAR(2)
+);
+
+CREATE TABLE geolocation (
+    geolocation_zip_code_prefix INT,
+    geolocation_lat DECIMAL(10,8),
+    geolocation_lng DECIMAL(11,8),
+    geolocation_city VARCHAR(100),
+    geolocation_state CHAR(2)
+);
