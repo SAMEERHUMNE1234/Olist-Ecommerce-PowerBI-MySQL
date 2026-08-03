ab8c60f3965122023fe2ba408553d1e183fcf7d5
Sameer Humane <sameergta143@gmail.com>
2026-08-03 11:39:46 +0530

    Add foreign key constraints for multiple tables
    Added foreign key constraints to establish relationships between orders, order_items, order_payments, customers, products, and sellers.

--- SQL/03_Foreign_Keys.sql
+++ SQL/03_Foreign_Keys.sql
@@ -1,1 +1,24 @@
+ALTER TABLE orders
+ADD CONSTRAINT fk_orders_customers
+FOREIGN KEY (customer_id)
+REFERENCES customers(customer_id);
 
+ALTER TABLE order_items
+ADD CONSTRAINT fk_order_items_orders
+FOREIGN KEY (order_id)
+REFERENCES orders(order_id);
+
+ALTER TABLE order_items
+ADD CONSTRAINT fk_order_items_products
+FOREIGN KEY (product_id)
+REFERENCES products(product_id);
+
+ALTER TABLE order_items
+ADD CONSTRAINT fk_order_items_sellers
+FOREIGN KEY (seller_id)
+REFERENCES sellers(seller_id);
+
+ALTER TABLE order_payments
+ADD CONSTRAINT fk_order_payments_orders
+FOREIGN KEY (order_id)
+REFERENCES orders(order_id);
